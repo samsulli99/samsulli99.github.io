72dcabc4a1b6f43d283e35dc9649c59b94586177
samsulli99 <sul15002@byui.edu>
2016-11-28 10:33:38 -0700

    secure
    making the http now https

--- weather/jsonweather-html/scripts/remote_wu.js
+++ weather/jsonweather-html/scripts/remote_wu.js
@@ -23,7 +23,7 @@ $(function () {
   // Get the data from the wunderground API
   function getData(lat, long){
     $.ajax({
-      url : "http://api.wunderground.com/api/58826cceab552ee6/geolookup/conditions/q/" + lat + ',' + long + ".json",
+      url : "https://api.wunderground.com/api/58826cceab552ee6/geolookup/conditions/q/" + lat + ',' + long + ".json",
       dataType : "jsonp",
       success : function(data) {
         console.log(data);
