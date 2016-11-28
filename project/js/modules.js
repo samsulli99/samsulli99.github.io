a221828701b395382077eb6bcd0999fc258270d0
samsulli99 <sul15002@byui.edu>
2016-11-28 15:51:39 -0700

    project-practice
    Adding Project Work

---
+++ project/js/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+  $("#page-header").load("/project/modules/header.html");
+  $("#page-nav").load("/project/modules/nav.html");
+  $("#page-footer").load("/project/modules/footer.html");
+});
